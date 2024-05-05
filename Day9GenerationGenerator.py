e909f3c4893fd38a191dfeda5c77023d89e5532f
Reem <rmkhattab93@gmail.com>
2024-05-05 01:23:42 -0700

    Create Day9GenerationGenerator.py
    Calculating what generation someone is in based on their birth year.

---
+++ Day9GenerationGenerator.py
@@ -0,0 +1,15 @@
+startingBirthYear = int(input("What year were you born? "))
+if startingBirthYear >= 1925 and startingBirthYear <= 1946:
+  print("You are part of the 'Traditionalists' generation.")
+elif startingBirthYear >= 1947 and startingBirthYear <= 1964:
+  print("You are part of the 'Baby Boomers' generation. You are the ones that ruined this country and you will all live in senior homes alone.")
+elif startingBirthYear >= 1965 and startingBirthYear <= 1981:
+  print("You are part of the 'Generation X' generation. You guys had it easy compared to your kids.")
+elif startingBirthYear >= 1982 and startingBirthYear <= 1995:
+  print("You are part of the 'Millenials' generation. You will never be able to afford a home, and you will work until you die.")
+elif startingBirthYear >= 1996 and startingBirthYear <= 2015:
+  print("You are part of the 'Generation Z' generation. You guys are so based.")
+elif startingBirthYear >= 2016 and startingBirthYear <= 2024:
+  print("You are part of the 'Generation Alpha' generation. The fate of the world rests in your hands.")
+else:
+  print("You are either an ancient vampire or a time traveler. Or you have yet to be born and are pulling our legs." )
