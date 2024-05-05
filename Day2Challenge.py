0ed3ef8778e661ba8bdadd56d2da9c7c9ee8945b
Reem <rmkhattab93@gmail.com>
2024-05-04 22:04:25 -0700

    Create Day2Challenge.py
    Call on an input, then take that input and store it in different variables, then print a full sentence calling on those variables.

---
+++ Day2Challenge.py
@@ -0,0 +1,10 @@
+myName = input("Hello, what is your name? ")
+myFood = input("What is your favorite food? ")
+myMusic = input("What is your favorite music? ")
+myState = input("Where do you live? ")
+print()
+print("Good morning, we hope your day has been superb!")
+print()
+print("You are "+ myName +"you are probably hungry for "+ myFood +" and you are definitely getting your groove on to "+ myMusic + "living in the amazing "+ myState +"!")
+
+#In this project you learn to create inputs and store the user's inputs in variables. 
